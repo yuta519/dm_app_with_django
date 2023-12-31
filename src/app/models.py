379d15e5132d062c7d6ec1900c68481975bdb659
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-12-31 11:44:26 -0800

    Create models for app
    Creatge User, Message and Conversation models

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django_stubs_ext.db.models import TypedModelMeta
 
-# Create your models here.
+
+class User(models.Model):
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+
+    class Meta(TypedModelMeta):
+        ordering = ["-created_at"]
+        db_table = "users"
+
+
+class Message(models.Model):
+    content: models.TextField = models.TextField()
+    user = models.ForeignKey(User, related_name="messages", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta(TypedModelMeta):
+        ordering = ["-created_at"]
+        db_table = "messages"
+
+
+class Conversation(models.Model):
+    user1 = models.ForeignKey(User, related_name="user1", on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, related_name="user2", on_delete=models.CASCADE)
+    # If we want to store a list of users, we can use JSONField
+    # users = models.JSONField() # type: models.JSONField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta(TypedModelMeta):
+        ordering = ["-created_at"]
+        db_table = "conversations"
