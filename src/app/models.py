8e7d816aa220045b807456559e956c5e93b54836
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-12-31 12:50:06 -0800

    Create BaseModel
    To extract common fields for models, create abstract model

--- src/app/models.py
+++ src/app/models.py
@@ -2,37 +2,42 @@ from django.db import models
 from django_stubs_ext.db.models import TypedModelMeta
 
 
-class User(models.Model):
+class BaseModel(models.Model):
+    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel):
     first_name: str
     last_name: str
     email: str
     password: str
-    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
-    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
     class Meta(TypedModelMeta):
         ordering = ["-created_at"]
         db_table = "users"
 
 
-class Message(models.Model):
+class Message(BaseModel):
     content: models.TextField = models.TextField()
     user = models.ForeignKey(User, related_name="messages", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta(TypedModelMeta):
         ordering = ["-created_at"]
         db_table = "messages"
 
 
-class Conversation(models.Model):
+class Conversation(BaseModel):
     user1 = models.ForeignKey(User, related_name="user1", on_delete=models.CASCADE)
     user2 = models.ForeignKey(User, related_name="user2", on_delete=models.CASCADE)
     # If we want to store a list of users, we can use JSONField
     # users = models.JSONField() # type: models.JSONField
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta(TypedModelMeta):
         ordering = ["-created_at"]
