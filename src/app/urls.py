6b84b3bc670a388676b53c8e08deae83f460681f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-02 15:28:22 -0800

    Create new path, /user/pk

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,14 +1,14 @@
 # Create routes for the views
 
 from django.urls import path
-from app.views import UserListView
+from app.views import UserDetailView, UserListView
 
 app_name = "app"
 
 urlpatterns = [
     # path("", views.index, name="index"),
     path("users/", UserListView.as_view(), name="user_list"),
-    # path("users/<int:pk>/", views.UserDetail.as_view()),
+    path("users/<int:pk>/", UserDetailView.as_view(), name="user_detail"),
     # path("messages/", views.MessageList.as_view()),
     # path("messages/<int:pk>/", views.MessageDetail.as_view()),
     # path("conversations/", views.ConversationList.as_view()),
--- src/app/views/user.py
+++ src/app/views/user.py
@@ -6,3 +6,8 @@ from app.models import User
 class UserListView(generic.ListView):
     model = User
     template_name = "users/list.html"
+
+
+class UserDetailView(generic.DetailView):
+    model = User
+    template_name = "users/detail.html"
