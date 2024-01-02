9267aa279b4acc2ef70c153a61377e843fa21daa
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-02 12:49:11 -0800

    Change user model
    The previous model does not generate name and email as table tbl.

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -1,13 +1,13 @@
+import django.db.models as models
 from django_stubs_ext.db.models import TypedModelMeta
 
 from app.models import BaseModel
 
 
 class User(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    password: str
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, null=False, blank=False)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
