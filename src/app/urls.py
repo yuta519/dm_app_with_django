08013fc1b2534ae4d385689ccebd7c0e2fb86cc6
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-04 12:50:34 -0800

    Add name to converstions

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,16 +1,11 @@
-# Create routes for the views
-
 from django.urls import path
-from app.views import UserDetailView, UserListView
+from app.views import ConversationListView, UserDetailView, UserListView
 
 app_name = "app"
 
 urlpatterns = [
-    # path("", views.index, name="index"),
     path("users/", UserListView.as_view(), name="user_list"),
     path("users/<int:pk>/", UserDetailView.as_view(), name="user_detail"),
-    # path("messages/", views.MessageList.as_view()),
-    # path("messages/<int:pk>/", views.MessageDetail.as_view()),
-    # path("conversations/", views.ConversationList.as_view()),
+    path("conversations/", ConversationListView.as_view(), name="conversation_list"),
     # path("conversations/<int:pk>/", views.ConversationDetail.as_view()),
 ]
