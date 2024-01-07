5f4fe31ade36c7ebe355694b24b8dead1da0603b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-06 21:49:34 -0800

    Create exits method for conversation model
    To validate duplicate user conversations, create new method exits as model class method

--- src/app/models/conversation.py
+++ src/app/models/conversation.py
@@ -1,20 +1,22 @@
-from django.db import models
+from django.db.models import CASCADE, ForeignKey, Q
 from django_stubs_ext.db.models import TypedModelMeta
 
 from app.models import BaseModel
 
 
 class Conversation(BaseModel):
-    user1 = models.ForeignKey(
-        "app.User", related_name="user1", on_delete=models.CASCADE
-    )
-    user2 = models.ForeignKey(
-        "app.User", related_name="user2", on_delete=models.CASCADE
-    )
+    user1 = ForeignKey("app.User", related_name="user1", on_delete=CASCADE)
+    user2 = ForeignKey("app.User", related_name="user2", on_delete=CASCADE)
     # If we want to store a list of users, we can use JSONField
-    # users = models.JSONField() # type: models.JSONField
+    # users = JSONField() # type: JSONField
 
     class Meta(TypedModelMeta):
         ordering = ["-created_at"]
         db_table = "conversations"
         app_label = "app"
+
+    def exists(users: list):
+        return Conversation.objects.filter(
+            Q(Q(user1=users[0]) & Q(user2=users[1]))
+            | Q(Q(user1=users[1]) & Q(user2=users[0]))
+        ).exists()
