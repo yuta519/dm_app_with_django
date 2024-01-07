1971a560890559c1e1ec6ae1ad35675ccef7e2a7
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-06 21:51:40 -0800

    Add validation
    As conversation serializer, add validation not to duplicate converation by users

--- src/app/serializers/conversations.py
+++ src/app/serializers/conversations.py
@@ -8,3 +8,14 @@ class ConversationSerializer(serializers.ModelSerializer):
         model = Conversation
         fields = ("user1", "user2", "created_at", "updated_at")
         read_only_fields = ("created_at", "updated_at")
+
+    def validate(self, data):
+        user1 = data.get("user1")
+        user2 = data.get("user2")
+
+        if Conversation.exists(users=[user1, user2]):
+            raise serializers.ValidationError(
+                "A conversation between these users already exists."
+            )
+
+        return data
