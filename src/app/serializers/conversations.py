d321e4baec342dd07393c1b621f977f06bc25e48
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-07 22:53:30 -0800

    Fix serializer
    To make error message readable, validationerror args become json. Serializer filed must include field

--- src/app/serializers/conversations.py
+++ src/app/serializers/conversations.py
@@ -6,16 +6,14 @@ from app.models import Conversation
 class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
-        fields = ("user1", "user2", "created_at", "updated_at")
+        fields = ("id", "user1", "user2", "created_at", "updated_at")
         read_only_fields = ("created_at", "updated_at")
 
     def validate(self, data):
         user1 = data.get("user1")
         user2 = data.get("user2")
-
         if Conversation.exists(users=[user1, user2]):
             raise serializers.ValidationError(
-                "A conversation between these users already exists."
+                {"error": "A conversation between these users already exists."}
             )
-
         return data
