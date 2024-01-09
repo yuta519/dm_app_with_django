ee3b1ed20487465b7510b13e62db740111db67e6
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-09 15:00:51 -0800

    Rever misconfiguration

--- src/config/settings.py
+++ src/config/settings.py
@@ -42,8 +42,6 @@ ROOT_URLCONF = "config.urls"
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
