967eccf5a5a4b0663a4e735fbfe114e4b1b5a9dc
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-11 13:52:13 -0800

    Add login path

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 from app.urls import urlpatterns as app_urls
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts", include("django.contrib.auth.urls")),
+    path("login/", auth_views.LoginView.as_view()),
     path("", include(app_urls)),
 ]
