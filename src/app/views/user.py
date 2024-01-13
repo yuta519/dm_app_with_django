4c41eff71097c94da695a1959a6592acb3280652
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-12 16:28:05 -0800

    Required authentication

--- src/app/views/user.py
+++ src/app/views/user.py
@@ -1,12 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.views import generic
 
 
-class UserListView(generic.ListView):
+class UserListView(LoginRequiredMixin, generic.ListView):
     model = User
     template_name = "users/list.html"
 
 
-class UserDetailView(generic.DetailView):
+class UserDetailView(LoginRequiredMixin, generic.DetailView):
     model = User
     template_name = "users/detail.html"
