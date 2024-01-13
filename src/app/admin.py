f3304ef9b8b79029b8354fc696a04388da610c67
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-13 15:39:43 -0800

    Create custom admin site class

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
 
-# Register your models here.
+from app.models import Conversation, Message, User
+
+
+class MyAdminSite(AdminSite):
+    site_title = "My Custom Admin Title"
+    site_header = "My Custom Admin Header"
+    index_title = "My Custom Admin Index Title"
+
+
+admin_site = MyAdminSite(name="myadmin")
+
+admin_site.register(Conversation)
+admin_site.register(Message)
+admin_site.register(User)
--- src/config/urls.py
+++ src/config/urls.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
 from django.urls import include, path
 
+from app.admin import admin_site
 from app.urls import urlpatterns as app_urls
 from app.views.auth import LoginView, logout_view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin_site.urls),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", logout_view, name="logout"),
     path("", include(app_urls)),
