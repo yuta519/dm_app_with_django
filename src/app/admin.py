ea22dc20b1ede5cf8a77f00b8366fc6abc3a7d25
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-14 20:11:26 -0800

    Admin site

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,16 +1,21 @@
-from django.contrib.admin import AdminSite
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin
 
-from app.models import Conversation, Message, User
+from app.models import Conversation
 
 
-class MyAdminSite(AdminSite):
+class MyAdminSite(admin.AdminSite):
     site_title = "My Custom Admin Title"
     site_header = "My Custom Admin Header"
     index_title = "My Custom Admin Index Title"
 
 
+class ConversationModelAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+
+
 admin_site = MyAdminSite(name="myadmin")
 
-admin_site.register(Conversation)
-admin_site.register(Message)
-admin_site.register(User)
+admin_site.register(Conversation, ConversationModelAdmin)
+admin_site.register(User, UserAdmin)
