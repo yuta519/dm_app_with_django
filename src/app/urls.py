6861dcb51af24bdc27fc66e0d6fde04e57c3e408
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-16 10:46:08 -0800

    Create conversation detail path

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from app.views import (
     ConversationApiViewSet,
+    ConversationDetailView,
     ConversationListView,
     UserDetailView,
     UserListView,
@@ -14,6 +15,11 @@ urlpatterns = [
     path("users/", UserListView.as_view(), name="user_list"),
     path("users/<int:pk>/", UserDetailView.as_view(), name="user_detail"),
     path("conversations/", ConversationListView.as_view(), name="conversation_list"),
+    path(
+        "conversations/<int:pk>/",
+        ConversationDetailView.as_view(),
+        name="conversation_detail",
+    ),
     path(
         "api/conversations",
         ConversationApiViewSet.as_view({"get": "list", "post": "create"}),
