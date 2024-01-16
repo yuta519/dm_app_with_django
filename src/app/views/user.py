101fb9016b91f2325bbcbdc8102e72b93aeb0d6f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-16 10:08:57 -0800

    Change context
    At user detail template, variable 'user' is conflicted between User model and Auth User model. Rename Auth User model to auth_user

--- src/app/views/user.py
+++ src/app/views/user.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.views import generic
@@ -11,3 +12,8 @@ class UserListView(LoginRequiredMixin, generic.ListView):
 class UserDetailView(LoginRequiredMixin, generic.DetailView):
     model = User
     template_name = "users/detail.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["auth_user"] = self.request.user
+        return context
