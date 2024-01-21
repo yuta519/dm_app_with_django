7d61e9ff385864581772b0339b25b583b7b6226b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-20 22:11:38 -0800

    Add user on socket connection
    Will use to distinguish sender

--- src/app/consumers.py
+++ src/app/consumers.py
@@ -1,8 +1,14 @@
 import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+# TODO: Will save messages to database
+# from app.models import Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    room_name = None
+
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         await self.channel_layer.group_add(self.room_name, self.channel_name)
@@ -11,21 +17,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_name, self.channel_layer)
 
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        # username = text_data_json["username"]
+    async def receive(self, received_data):
+        data = json.loads(received_data)
+        message, user = data["message"], data["user"]
         await self.channel_layer.group_send(
-            self.room_name,
-            {
-                "type": "sendMessage",
-                "message": message,
-                # "username" : username ,
-            },
+            self.room_name, {"message": message, "user": user}
         )
 
     async def sendMessage(self, event):
         message = event["message"]
-        # username = event["username"]
-        # await self.send(text_data = json.dumps({"message":message ,"username":username}))
-        await self.send(text_data=json.dumps({"message": message}))
+        user = event["user"]
+        await self.send(text_data=json.dumps({"message": message, "user": user}))
