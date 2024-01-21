5597a0293ce1b98ac16869b6bedb756b9fb4be07
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-20 22:27:18 -0800

    Fix path parameter variable

--- src/app/consumers.py
+++ src/app/consumers.py
@@ -3,26 +3,26 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 # TODO: Will save messages to database
-# from app.models import Message
+# from app.models import Conversation, Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    room_name = None
+    room_id = None
 
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        await self.channel_layer.group_add(self.room_name, self.channel_name)
+        self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
+        await self.channel_layer.group_add(self.room_id, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_name, self.channel_layer)
+        await self.channel_layer.group_discard(self.room_id, self.channel_layer)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         user = text_data_json["user"]
         await self.channel_layer.group_send(
-            self.room_name,
+            self.room_id,
             {
                 "type": "sendMessage",
                 "message": message,
