22bb13ada0d15cd43d24309b9ff26dfac75f3a1b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-23 12:42:39 -0800

    Add messages as conversation response

--- src/app/serializers/conversations.py
+++ src/app/serializers/conversations.py
@@ -4,10 +4,17 @@ from app.models import Conversation
 
 
 class ConversationSerializer(serializers.ModelSerializer):
+    messages = serializers.SerializerMethodField()
+
     class Meta:
         model = Conversation
-        fields = ("id", "user1", "user2", "created_at", "updated_at")
+        fields = ("id", "messages", "user1", "user2", "created_at", "updated_at")
         read_only_fields = ("created_at", "updated_at")
+
+    def get_messages(self, obj: Conversation):
+        from app.serializers import MessageSerializer
+
+        return MessageSerializer(obj.messages, many=True).data
 
     def validate(self, data):
         user1 = data.get("user1")
