8b7051f7ce1369f170816412c5409174e48b7a96
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-23 17:00:42 -0800

    Create friend field

--- src/app/views/conversation.py
+++ src/app/views/conversation.py
@@ -10,6 +10,18 @@ class ConversationListView(LoginRequiredMixin, generic.ListView):
     model = Conversation
     template_name = "conversations/list.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        conversations = Conversation.objects.all()
+        for conversation in conversations:
+            conversation.friend = (
+                conversation.user2
+                if conversation.user1 == self.request.user
+                else conversation.user1
+            )
+        context["conversation_list"] = conversations
+        return context
+
 
 class ConversationDetailView(LoginRequiredMixin, generic.DetailView):
     model = Conversation
