1af93b816c976644cce3e2f08296c5d95e9c2f4d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-24 14:05:35 -0800

    Exclude myself as queryset of users

--- src/app/views/user.py
+++ src/app/views/user.py
@@ -1,12 +1,16 @@
 from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.db.models.query import QuerySet
 from django.views import generic
 
 
 class UserListView(LoginRequiredMixin, generic.ListView):
     model = User
     template_name = "users/list.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return User.objects.exclude(id=self.request.user.id)
 
 
 class UserDetailView(LoginRequiredMixin, generic.DetailView):
