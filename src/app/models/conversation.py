173f900488e1f1b2fc7dcb2e05af9949e3030a6a
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-02-08 22:41:57 -0800

    Try Many to Many　between conversation and user

--- src/app/models/conversation.py
+++ src/app/models/conversation.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.db.models import CASCADE, ForeignKey, Q
+from django.db.models import CASCADE, ForeignKey, ManyToManyField, Q
 from django_stubs_ext.db.models import TypedModelMeta
 
 from app.models import BaseModel
@@ -9,7 +9,7 @@ class Conversation(BaseModel):
     user1 = ForeignKey(User, related_name="user1", on_delete=CASCADE)
     user2 = ForeignKey(User, related_name="user2", on_delete=CASCADE)
     # If we want to store a list of users, we can use JSONField
-    # users = JSONField() # type: JSONField
+    users = ManyToManyField(User, related_name="conversations")
 
     class Meta(TypedModelMeta):
         ordering = ["-created_at"]
